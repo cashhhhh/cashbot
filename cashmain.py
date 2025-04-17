bbe5dfb0aa7ee9d5f22d49ec69e7c4668849172a
cashhhhh <67353040+cashhhhh@users.noreply.github.com>
2025-04-17 17:48:06 -0500

    Update cashmain.py

--- cashmain.py
+++ cashmain.py
@@ -366,7 +366,7 @@ async def checkticket(ctx, amount: float, unread_only: bool = True):
                 logging.error(
                     f"Failed to send traffic alert to {user_id}: {e}")
 
-    allowed_role_ids = [1103522760073945168, 1325902622120738866, 1361045953296990490, 1332736087029710958, 1361231253596278794]
+    allowed_role_ids = [1103522760073945168, 1325902622120738866, 1361045953296990490, 1332736087029710958, 1361231253596278794, 1319913613389074487]
     is_owner = str(ctx.author.id) in OWNER_IDS
     has_role = any(role.id in allowed_role_ids for role in ctx.author.roles)
 
